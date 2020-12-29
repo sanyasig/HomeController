187c5806b9dc8484c2679f7a6d6c9a86f3da1001
nani <nani76.gsrao@gmail.com>
2020-12-29 16:07:13 +0000

    updated IP for firetv and rmlink

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 firetv = dict(
-                ip = '192.168.1.22',
+                ip = '192.168.1.36',
                 port = '5555',
                 start_youtube = ''
             )
 
 rm_broad_link = dict(
-        ip = '192.168.1.34',
+        ip = '192.168.1.35',
         port = '5555',
         start_youtube = ''
     )
